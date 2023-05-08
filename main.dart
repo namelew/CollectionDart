799b803ec1f01c40c7fcb032cdc34d6c09cfcfb7
namelew <diogomaciel.cunha@gmail.com>
2023-05-08 10:45:13 -0300

    utilizando um switch case para escolher o tipo de transporte

--- main.dart
+++ main.dart
@@ -2,18 +2,25 @@ void main(List<String> args) {
   chooseTransport(Transport.plane);
 }
 
-void chooseTransport(Transport transport) {
-  if (transport == Transport.car) {
-    print("Vou de carro");
-  } else if (transport == Transport.bike) {
-    print("Vou de bike");
-  } else {
-    print("vou para a aventura");
+void chooseTransport(Transport locomotion) {
+  switch (locomotion) {
+    case Transport.car:
+      print("Vou de carro para a aventura");
+      break;
+    case Transport.bike:
+      print("Vou de bike para a aventura");
+      break;
+    case Transport.bus:
+      print("Vou de ôninus para a aventura");
+      break;
+    default:
+      print("Estou indo para a aventura, isso é o que importa");
   }
 }
 
 enum Transport{
   car,
+  bus,
   bike,
   walk,
   skate,
